f783e049a1a7a62212dddc12db2167864ee3caff
Buddhinath Mahato <buddhinathmahato016@gmail.com>
2024-07-19 10:53:36 +0530

    updated week-1\01-js\hard\todo-list.js

--- week-1/01-js/hard/todo-list.js
+++ week-1/01-js/hard/todo-list.js
@@ -13,7 +13,56 @@
 */
 
 class Todo {
+  constructor(){
+    this.todos = [];
+  }
 
+  add(todo){
+    return this.todos.push(todo);
+  }
+
+  remove(index){
+    if(index<0 || index>this.todos.length){
+      return "Index out of length";
+    }
+    return this.todos.splice(index,1);
+  }
+
+  update(index, updatedTodo){
+    if(index<0 || index > this.todos.length){
+      return "Index out of length";
+    }
+    this.todos[index] = updatedTodo;
+    return this;
+  }
+
+  getAll(){
+    return this.todos;
+  } 
+
+  get(indexOfTodo){
+    return this.todos[indexOfTodo];
+  }
+
+  clear(){
+    return this.todos.length = 0;
+  }
 }
 
-module.exports = Todo;
+// Example usage:
+const todoList = new Todo();
+
+todoList.add("Buy groceries");
+todoList.add("Walk the dog");
+todoList.add("Pay bills");
+console.log(todoList.getAll()); // Outputs: ["Buy groceries", "Walk the dog", "Pay bills"]
+
+todoList.update(1, "Walk the dog in the park");
+console.log(todoList.getAll()); // Outputs: ["Buy groceries", "Walk the dog in the park", "Pay bills"]
+
+todoList.remove(2);
+console.log(todoList.getAll()); // Outputs: ["Buy groceries", "Walk the dog in the park"]
+
+console.log(todoList.get(0)); // Outputs: "Buy groceries"
+
+// module.exports = Todo;
